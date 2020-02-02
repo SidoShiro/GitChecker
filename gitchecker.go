c1897c313f68c99f07093ad5adf10b5bd486a142
sidore_m <marc@sidorenko.fr>
2020-02-02 17:47:34 +0100

    Fix

--- gitchecker.go
+++ gitchecker.go
@@ -12,7 +12,7 @@ import (
 	"strings"
 )
 
-type funcPrint func(str string)
+type funcPrint func(s string)
 
 type summary struct {
 	countGits         int
@@ -33,8 +33,7 @@ func printList(printFunc funcPrint, listGits []string) {
 }
 
 func Summary(cSum *summary, showDetail bool) {
-
-	shell_color.BluePrintln("Summary: (over the " + strconv.Itoa(cSum.countGits) + " repositories)")
+	shell_color.BluePrint("Summary: (over the " + strconv.Itoa(cSum.countGits) + " repositories)\n")
 	shell_color.GreenPrint("\tUpdated " + strconv.Itoa(cSum.countUpdated) + "/" + strconv.Itoa(cSum.countGits))
 	if showDetail {
 		if len(cSum.listUpdatedGits) != 0 {
@@ -86,28 +85,28 @@ func AddToChecker(path string, cSum *summary, optionVerbose bool) {
 	if strings.Contains(st, "Your branch is up to date with") {
 		cSum.listUpdatedGits = append(cSum.listUpdatedGits, path)
 		if optionVerbose {
-			shell_color.GreenPrintln("\t\t* Local changes updated with remotes")
+			shell_color.GreenPrint("\t\t* Local changes updated with remotes\n")
 		}
 		cSum.countUpdated++
 	}
 	if strings.Contains(st, "Your branch is ahead of") {
 		cSum.listUnpushedGits = append(cSum.listUnpushedGits, path)
 		if optionVerbose {
-			shell_color.CyanPrintln("\t\t* Unpushed commits")
+			shell_color.CyanPrint("\t\t* Unpushed commits\n")
 		}
 		cSum.countUnpushed++
 	}
 	if strings.Contains(st, "Changes not staged for commit") {
 		cSum.listChangesGits = append(cSum.listChangesGits, path)
 		if optionVerbose {
-			shell_color.RedPrintln("\t\t* Has changes not staged for commit")
+			shell_color.RedPrint("\t\t* Has changes not staged for commit\n")
 		}
 		cSum.countChanges++
 	}
 	if strings.Contains(st, "Untracked files:") {
 		cSum.listUntrackedGits = append(cSum.listUntrackedGits, path)
 		if optionVerbose {
-			shell_color.YellowPrintln("\t\t* Has untracked files")
+			shell_color.YellowPrint("\t\t* Has untracked files\n")
 		}
 		cSum.countUntracked++
 	}
